3d43d02ff144fde61c43cf4c30f2f400ed1b31b2
TιJ <78140568+TiJ-code@users.noreply.github.com>
2026-06-26 19:01:02 +0200

    Added Discord Message Publication
    Added discord thread that publishes enqueued messages.

---
+++ src/main/java/tij/bot/trello2discord/common/Constants.java
@@ -0,0 +1,7 @@
+package tij.bot.trello2discord.common;
+
+public final class Constants {
+    public static int COLOR_TRELLO_BLUE = 0x0079BF;
+
+    private Constants() {}
+}
---
+++ src/main/java/tij/bot/trello2discord/discord/DiscordWorker.java
@@ -0,0 +1,81 @@
+package tij.bot.trello2discord.discord;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import tij.bot.trello2discord.common.Constants;
+import tij.bot.trello2discord.common.Message;
+import tij.bot.trello2discord.common.MessageBuffer;
+import tij.bot.trello2discord.config.Config;
+import tij.bot.trello2discord.config.ConfigUserMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+public class DiscordWorker {
+
+    private final JDA jda;
+    private final String channelId;
+    private final MessageBuffer buffer;
+    private final List<ConfigUserMapping> userMappings;
+
+    public DiscordWorker(Config config, MessageBuffer buffer) throws InterruptedException {
+        this.channelId = config.discordChannelId();
+        this.userMappings = config.discordTrelloUserMapping();
+        this.buffer = buffer;
+
+        System.out.println("Connecting to Discord...");
+        this.jda = JDABuilder.createDefault(config.discordBotToken()).build().awaitReady();
+        System.out.println("Discord connected successfully.");
+    }
+
+    public void startConsuming() {
+        Thread consumerThread = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    Message msg = buffer.pop();
+                    publish(msg);
+                } catch (InterruptedException e) {
+                    System.err.println("Discord worker thread interupted.");
+                    Thread.currentThread().interrupt();
+                    break;
+                } catch (Exception e) {
+                    System.err.println("Error publishing to Discord: " + e.getMessage());
+                }
+            }
+        });
+
+        consumerThread.setDaemon(true);
+        consumerThread.setName("Discord-Publisher-Thread");
+        consumerThread.start();
+    }
+
+    private void publish(Message msg) {
+        TextChannel channel = jda.getTextChannelById(channelId);
+        if (channel == null) {
+            System.err.println("Error: Could not find Discord channel with ID: " + channelId);
+            return;
+        }
+
+        String discordAuthorId = userMappings.stream()
+                .filter(m -> m.trelloUserId().equals(msg.getAuthorName()))
+                .map(ConfigUserMapping::discordUserId)
+                .limit(1)
+                .toString();
+
+        User discordAuthor = jda.getUserById(Long.parseLong(discordAuthorId));
+
+        EmbedBuilder embed = new EmbedBuilder()
+                .setColor(Constants.COLOR_TRELLO_BLUE)
+                .setAuthor(discordAuthor.getEffectiveName(), discordAuthor.getAsMention(), msg.getAuthorAvatarUrl())
+                .setTitle(msg.getTitle())
+                .setDescription(msg.getBody())
+                .setFooter("Trello • " + msg.getBoardName(), "https://www.vectorlogo.zone/logos/trello/trello-icon.svg");
+
+        msg.getFields().forEach((k, v) -> embed.addField(k, v, false));
+
+        channel.sendMessageEmbeds(embed.build()).queue();
+    }
+}
