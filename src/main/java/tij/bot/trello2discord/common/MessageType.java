8818a4e4ce6befdd592cad206ef9c856409b48b1
TιJ <78140568+TiJ-code@users.noreply.github.com>
2026-06-26 19:40:44 +0200

    Refactored Message Creation Logic

--- src/main/java/tij/bot/trello2discord/common/MessageType.java
+++ src/main/java/tij/bot/trello2discord/common/MessageType.java
@@ -1,8 +1,45 @@
 package tij.bot.trello2discord.common;
 
 public enum MessageType {
-    CARD_CREATED,
-    CARD_COMMENTED,
-    CARD_MOVED,
-    GENERIC_UPDATE
+    CARD_CREATED(
+            "📥 New Card Created",
+            "%s",
+            MessageField.of("List", "%s")
+    ),
+    CARD_COMMENTED(
+            "💬 New Comment on Card",
+            "\n\n**Comment:** %s"
+    ),
+    CARD_MOVED(
+            "🚚 Card Moved",
+            "%s",
+            MessageField.of("From", "%s"),
+            MessageField.of("To", "%s")
+    ),
+    GENERIC_UPDATE(
+            "✏️ Card Updated",
+            "%s"
+    );
+
+    private final String titleMessage;
+    private final String bodyFormat;
+    private final MessageField[] fields;
+
+    MessageType(String titleMessage, String bodyFormat, MessageField... fields) {
+        this.titleMessage = titleMessage;
+        this.bodyFormat = bodyFormat;
+        this.fields = fields;
+    }
+
+    public String getTitleMessage() {
+        return titleMessage;
+    }
+
+    public String getBodyFormat(Object... formatObjects) {
+        return bodyFormat.formatted(formatObjects);
+    }
+
+    public MessageField[] getFields() {
+        return fields;
+    }
 }
