3f79dc82c193bfc1e36357109645e26d69fc1021
TιJ <78140568+TiJ-code@users.noreply.github.com>
2026-06-27 09:29:48 +0200

    Added Label Events To Webhook Refererer
    Added "addedLabelToCard" and "removedLabelFromCard" event to Trello webhook.

--- src/main/java/tij/bot/trello2discord/trello/utils/JsonConstants.java
+++ src/main/java/tij/bot/trello2discord/trello/utils/JsonConstants.java
@@ -6,6 +6,7 @@ public final class JsonConstants {
     public static final String OBJECT_DATA = "data";
     public static final String OBJECT_BOARD = "board";
     public static final String OBJECT_CARD = "card";
+    public static final String OBJECT_LABEL = "label";
     public static final String OBJECT_LIST = "list";
     public static final String OBJECT_LIST_BEFORE = "listBefore";
     public static final String OBJECT_LIST_AFTER = "listAfter";
@@ -21,6 +22,8 @@ public final class JsonConstants {
     public static final String EVENT_CREATE_CARD = "createCard";
     public static final String EVENT_COMMENT_CARD = "commentCard";
     public static final String EVENT_UPDATE_CARD = "updateCard";
+    public static final String EVENT_ADDED_LABEL_TO_CARD = "addLabelToCard";
+    public static final String EVENT_REMOVED_LABEL_FROM_CARD = "removeLabelFromCard";
 
 
     private JsonConstants() {}
